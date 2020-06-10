a26a60205c0c9cef5e6d1e7a89a923f87bc99bc3
Mozer <fabricio.mozer@accenture.com>
2020-06-09 22:41:32 -0300

    Commit inicial

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,97 @@
 import React, {Component} from 'react';
-
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'JSHunt',
+  };
+
+  state = {
+    docs: [],
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/products');
+
+    const {docs} = response.data;
+
+    this.setState({docs});
+    //console.log(docs);
+  };
+  renderItem = ({item}) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productsTitle}>{item.title}</Text>
+      <Text style={styles.productsDesciption}>{item.description}</Text>
+
+      <TouchableOpacity style={styles.productionButton} onPress={() => {}}>
+        <Text style={styles.productButtonText}>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
   render() {
     return (
-      <View>
-        <Text>Página Main</Text>
+      <View style={styles.container}>
+        {/* <Text>Página Main:</Text>
+        {this.state.docs.map(product => (
+          <Text key={product._id}>{product.title}</Text>
+        ))} */}
+
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  list: {
+    padding: 20,
+  },
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+  productsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  productsDesciption: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+  productionButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold',
+  },
+});
